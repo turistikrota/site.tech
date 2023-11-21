f4cce0c19b58bf163e746e8154c92cd936063bf9
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-21 22:10:36 +0300

    feature: add site base

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,28 @@
 import DefaultLayout from "../layouts/default";
+import BlogSection from "./components/BlogSection";
+import ClientsSection from "./components/ClientsSection";
+import CountSection from "./components/CountSection";
+import FaqSection from "./components/FaqSection";
+import FeaturesSection from "./components/FeaturesSection";
+import HeadSection from "./components/HeadSection";
+import HiringSection from "./components/HiringSection";
+import IntegrationSection from "./components/IntegrationSection";
+import PricingDetailedSection from "./components/PricingDetailedSection";
+import WhatWeDoSection from "./components/WhatWeDoSection";
 
 export default function Home() {
   return (
     <DefaultLayout>
-      saa
+      <HeadSection />
+      <WhatWeDoSection />
+      <PricingDetailedSection />
+      <CountSection />
+      <ClientsSection />
+      <IntegrationSection />
+      <FeaturesSection />
+      <BlogSection />
+      <HiringSection />
+      <FaqSection />
     </DefaultLayout>
   )
 }
