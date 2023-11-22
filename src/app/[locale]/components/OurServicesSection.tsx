d138af5807c19260eeb05a2ff48c327ba94075b2
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-22 22:20:15 +0300

    fix our services section

--- src/app/[locale]/components/OurServicesSection.tsx
+++ src/app/[locale]/components/OurServicesSection.tsx
@@ -1,6 +1,45 @@
+type Item = {
+  title: string
+  description: string
+  icon: string
+}
+
+const Items: Item[] = [
+  {
+    title: 'Drive More Sales',
+    description: `If the distribution of letters and 'words' is random, the reader will not be distracted from making.`,
+    icon: 'bx-heart',
+  },
+  {
+    title: 'Drive More Sales',
+    description: `If the distribution of letters and 'words' is random, the reader will not be distracted from making.`,
+    icon: 'bx-heart',
+  },
+  {
+    title: 'Drive More Sales',
+    description: `If the distribution of letters and 'words' is random, the reader will not be distracted from making.`,
+    icon: 'bx-heart',
+  },
+  {
+    title: 'Drive More Sales',
+    description: `If the distribution of letters and 'words' is random, the reader will not be distracted from making.`,
+    icon: 'bx-heart',
+  },
+  {
+    title: 'Drive More Sales',
+    description: `If the distribution of letters and 'words' is random, the reader will not be distracted from making.`,
+    icon: 'bx-heart',
+  },
+  {
+    title: 'Drive More Sales',
+    description: `If the distribution of letters and 'words' is random, the reader will not be distracted from making.`,
+    icon: 'bx-heart',
+  },
+]
+
 export default function OurServicesSection() {
   return (
-    <section className='relative bg-gray-50 py-16 dark:bg-slate-800 md:py-24' id='features'>
+    <section className='relative mx-auto max-w-7xl py-16 md:py-24' id='features'>
       <div className='lg container mx-auto'>
         <div className='grid grid-cols-1 pb-8 text-center'>
           <h3 className='mb-4 text-xl font-medium md:text-2xl'>Our Services</h3>
@@ -9,180 +48,38 @@ export default function OurServicesSection() {
           </p>
         </div>
         <div className='mt-4 grid grid-cols-1 gap-[30px] md:grid-cols-2 lg:grid-cols-3'>
-          <div className='group relative mt-4 overflow-hidden rounded-xl text-center transition duration-500 ease-in-out lg:px-6'>
-            <div className='relative -m-3 overflow-hidden text-transparent'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                stroke-width='2'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                className='mx-auto h-28 w-28 rotate-[30deg] fill-violet-600/5'
-              >
-                <path d='M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z'></path>
-              </svg>
-              <div className='absolute end-0 start-0 top-2/4 mx-auto flex -translate-y-2/4 items-center justify-center rounded-xl align-middle text-3xl text-violet-600 transition duration-500 ease-in-out'>
-                <i className='uil uil-adjust-circle'></i>
+          {Items.map((item, idx) => (
+            <div
+              key={idx}
+              className='group relative mt-4 overflow-hidden rounded-xl text-center transition duration-500 ease-in-out lg:px-6'
+            >
+              <div className='relative -m-3 overflow-hidden text-transparent'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='24'
+                  height='24'
+                  viewBox='0 0 24 24'
+                  fill='none'
+                  stroke='currentColor'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  className='mx-auto h-28 w-28 rotate-[30deg] fill-white/5'
+                >
+                  <path d='M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z'></path>
+                </svg>
+                <div className='absolute end-0 start-0 top-2/4 mx-auto flex -translate-y-2/4 items-center justify-center rounded-xl align-middle text-3xl text-violet-600 transition duration-500 ease-in-out'>
+                  <i className={`bx ${item.icon}`}></i>
+                </div>
+              </div>
+              <div className='mt-6'>
+                <a className='h5 text-lg transition duration-500 ease-in-out hover:text-violet-600' href='/services'>
+                  {item.title}
+                </a>
+                <p className='mt-3 text-slate-400 transition duration-500 ease-in-out'>{item.description}</p>
               </div>
             </div>
-            <div className='mt-6'>
-              <a className='h5 text-lg transition duration-500 ease-in-out hover:text-violet-600' href='/services'>
-                Grow Your Business
-              </a>
-              <p className='mt-3 text-slate-400 transition duration-500 ease-in-out'>
-                If the distribution of letters and 'words' is random, the reader will not be distracted from making.
-              </p>
-            </div>
-          </div>
-          <div className='group relative mt-4 overflow-hidden rounded-xl text-center transition duration-500 ease-in-out lg:px-6'>
-            <div className='relative -m-3 overflow-hidden text-transparent'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                stroke-width='2'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                className='mx-auto h-28 w-28 rotate-[30deg] fill-violet-600/5'
-              >
-                <path d='M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z'></path>
-              </svg>
-              <div className='absolute end-0 start-0 top-2/4 mx-auto flex -translate-y-2/4 items-center justify-center rounded-xl align-middle text-3xl text-violet-600 transition duration-500 ease-in-out'>
-                <i className='uil uil-circuit'></i>
-              </div>
-            </div>
-            <div className='mt-6'>
-              <a className='h5 text-lg transition duration-500 ease-in-out hover:text-violet-600' href='/services'>
-                Drive More Sales
-              </a>
-              <p className='mt-3 text-slate-400 transition duration-500 ease-in-out'>
-                If the distribution of letters and 'words' is random, the reader will not be distracted from making.
-              </p>
-            </div>
-          </div>
-          <div className='group relative mt-4 overflow-hidden rounded-xl text-center transition duration-500 ease-in-out lg:px-6'>
-            <div className='relative -m-3 overflow-hidden text-transparent'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                stroke-width='2'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                className='mx-auto h-28 w-28 rotate-[30deg] fill-violet-600/5'
-              >
-                <path d='M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z'></path>
-              </svg>
-              <div className='absolute end-0 start-0 top-2/4 mx-auto flex -translate-y-2/4 items-center justify-center rounded-xl align-middle text-3xl text-violet-600 transition duration-500 ease-in-out'>
-                <i className='uil uil-fire'></i>
-              </div>
-            </div>
-            <div className='mt-6'>
-              <a className='h5 text-lg transition duration-500 ease-in-out hover:text-violet-600' href='/services'>
-                Handled By Expert
-              </a>
-              <p className='mt-3 text-slate-400 transition duration-500 ease-in-out'>
-                If the distribution of letters and 'words' is random, the reader will not be distracted from making.
-              </p>
-            </div>
-          </div>
-          <div className='group relative mt-4 overflow-hidden rounded-xl text-center transition duration-500 ease-in-out lg:px-6'>
-            <div className='relative -m-3 overflow-hidden text-transparent'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                stroke-width='2'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                className='mx-auto h-28 w-28 rotate-[30deg] fill-violet-600/5'
-              >
-                <path d='M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z'></path>
-              </svg>
-              <div className='absolute end-0 start-0 top-2/4 mx-auto flex -translate-y-2/4 items-center justify-center rounded-xl align-middle text-3xl text-violet-600 transition duration-500 ease-in-out'>
-                <i className='uil uil-flower'></i>
-              </div>
-            </div>
-            <div className='mt-6'>
-              <a className='h5 text-lg transition duration-500 ease-in-out hover:text-violet-600' href='/services'>
-                Increase Conversion
-              </a>
-              <p className='mt-3 text-slate-400 transition duration-500 ease-in-out'>
-                If the distribution of letters and 'words' is random, the reader will not be distracted from making.
-              </p>
-            </div>
-          </div>
-          <div className='group relative mt-4 overflow-hidden rounded-xl text-center transition duration-500 ease-in-out lg:px-6'>
-            <div className='relative -m-3 overflow-hidden text-transparent'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                stroke-width='2'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                className='mx-auto h-28 w-28 rotate-[30deg] fill-violet-600/5'
-              >
-                <path d='M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z'></path>
-              </svg>
-              <div className='absolute end-0 start-0 top-2/4 mx-auto flex -translate-y-2/4 items-center justify-center rounded-xl align-middle text-3xl text-violet-600 transition duration-500 ease-in-out'>
-                <i className='uil uil-shopping-basket'></i>
-              </div>
-            </div>
-            <div className='mt-6'>
-              <a className='h5 text-lg transition duration-500 ease-in-out hover:text-violet-600' href='/services'>
-                Drive More Sales
-              </a>
-              <p className='mt-3 text-slate-400 transition duration-500 ease-in-out'>
-                If the distribution of letters and 'words' is random, the reader will not be distracted from making.
-              </p>
-            </div>
-          </div>
-          <div className='group relative mt-4 overflow-hidden rounded-xl text-center transition duration-500 ease-in-out lg:px-6'>
-            <div className='relative -m-3 overflow-hidden text-transparent'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                stroke-width='2'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                className='mx-auto h-28 w-28 rotate-[30deg] fill-violet-600/5'
-              >
-                <path d='M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z'></path>
-              </svg>
-              <div className='absolute end-0 start-0 top-2/4 mx-auto flex -translate-y-2/4 items-center justify-center rounded-xl align-middle text-3xl text-violet-600 transition duration-500 ease-in-out'>
-                <i className='uil uil-flip-h'></i>
-              </div>
-            </div>
-            <div className='mt-6'>
-              <a className='h5 text-lg transition duration-500 ease-in-out hover:text-violet-600' href='/services'>
-                Sales Growth Idea
-              </a>
-              <p className='mt-3 text-slate-400 transition duration-500 ease-in-out'>
-                If the distribution of letters and 'words' is random, the reader will not be distracted from making.
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
