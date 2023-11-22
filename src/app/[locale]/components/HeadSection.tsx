e5db284246b5050c176eb13500a709657bba6681
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-23 00:05:58 +0300

    fix head section

--- src/app/[locale]/components/HeadSection.tsx
+++ src/app/[locale]/components/HeadSection.tsx
@@ -1,6 +1,8 @@
+import { useTranslations } from 'next-intl'
 import Link from 'next/link'
 
 export default function HeadSection() {
+  const t = useTranslations('home.head')
   return (
     <section className='relative mx-auto max-w-7xl px-4 py-48 xl:px-0'>
       <div className='hero-with-shapes'>
@@ -11,19 +13,16 @@ export default function HeadSection() {
         <div>
           <h1 className='my-5 text-5xl font-medium md:text-6xl'>
             <span className='bg-gradient-to-r from-fuchsia-500 to-teal-500 bg-clip-text text-transparent'>
-              We design user experiences that works
+              {t('title')}
             </span>
           </h1>
-          <p className='mb-20 mt-6 w-3/4 text-lg font-medium text-slate-600 dark:text-slate-400'>
-            We're a top-notch web design and development team helping business to craft the meaningful and interactive
-            product experiences.
-          </p>
+          <p className='mb-20 mt-6 w-3/4 text-lg font-medium text-slate-600 dark:text-slate-400'>{t('subtitle')}</p>
           <div className='flex flex-wrap items-center gap-5'>
             <Link
               className='animate-fade-in rounded-md bg-gray-400/5 px-6 py-3 transition-all duration-200 hover:bg-gray-400/10 dark:bg-white/5 dark:hover:bg-white/10'
               href='mailto:tech@turistikrota.com'
             >
-              Contact Us
+              {t('contact')}
             </Link>
           </div>
         </div>
