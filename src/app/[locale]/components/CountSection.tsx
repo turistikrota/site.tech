c6b05854ff9225f11e22be63d4b160e770837139
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-23 00:10:57 +0300

    fix count section

--- src/app/[locale]/components/CountSection.tsx
+++ src/app/[locale]/components/CountSection.tsx
@@ -1,43 +1,42 @@
+import { useTranslations } from 'next-intl'
 import Section from './Section'
 
 type Item = {
   title: string
-  count: number
+  count: string
 }
 
 const Items: Item[] = [
   {
-    title: 'Meeting Rooms',
-    count: 21,
+    title: 'data.success',
+    count: '50+',
   },
   {
-    title: 'Event Spaces',
-    count: 51,
+    title: 'data.satisfied',
+    count: '100+',
   },
   {
-    title: 'Studio Rooms',
-    count: 11,
+    title: 'data.innovative',
+    count: '10+',
   },
   {
-    title: 'Seating Spaces',
-    count: 500,
+    title: 'data.support',
+    count: '7/24',
   },
 ]
 
 export default function CountSection() {
+  const t = useTranslations('home.count')
   return (
     <Section>
-      <Section.Head
-        title='More Productivity, Less Expenses'
-        subtitle='From an established enterprise or a startup, we offer space that fits all.'
-      />
+      <Section.Head title={t('title')} subtitle={t('subtitle')} />
       <div className='aos-init aos-animate mt-14 grid grid-cols-2 gap-5 md:grid-cols-4' data-aos='fade-up'>
         {Items.map((item, idx) => (
           <div key={idx} className='text-center'>
             <h4 className='mb-3 text-5xl '>
               <span className={`${idx % 2 === 0 ? 'text-fuchsia-500' : 'text-teal-500'}`}>{item.count}</span>
             </h4>
-            <p className='text-slate-600 dark:text-slate-300'>{item.title}</p>
+            <p className='text-slate-600 dark:text-slate-300'>{t(`${item.title}`)}</p>
           </div>
         ))}
       </div>
